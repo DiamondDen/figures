12e810d8e0afd8727eb34d1ee30daafd33ff20e6
hal10000 <3389179@gmail.com>
2022-10-28 20:37:00 +0700

    add lessThan, moreThan, notEquals,  equals

--- src/main/java/net/dd/sem1/figure/Figure.java
+++ src/main/java/net/dd/sem1/figure/Figure.java
@@ -11,5 +11,28 @@ public abstract class Figure {
     System.out.println(this.toString());
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Figure)) return false;
+    if (obj == this) return true;
+    return ((Figure) obj).calcArea() == this.calcArea();
+  }
+
+  public boolean notEquals(Object obj) {
+    return !this.equals(obj);
+  }
+
+  public boolean moreThan(Object obj) {
+    if (!(obj instanceof Figure)) return false;
+    if (obj == this) return false;
+    return this.calcArea() > ((Figure) obj).calcArea();
+  }
+
+  public boolean lessThan(Object obj) {
+    if (!(obj instanceof Figure)) return false;
+    if (obj == this) return false;
+    return this.calcArea() < ((Figure) obj).calcArea();
+  }
+
   public abstract String toString();
 }
